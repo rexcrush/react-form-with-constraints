93e0fd232b6c47f57645fff752eab6721f0978a9
Tanguy Krotoff <tkrotoff@gmail.com>
2018-10-17 00:48:58 +0200

    i18next-parser 1.0.0

---
+++ examples/SignUp/i18next-parser.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+
+module.exports = {
+  locales: ['fr'],
+  output: 'locales/$LOCALE/$NAMESPACE.json',
+
+  // See https://github.com/i18next/i18next-parser/issues/129
+  namespaceSeparator: '__NS',
+  keySeparator: '__KS'
+};
